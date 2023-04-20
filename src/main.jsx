1cf3e7ec3005614d4769d7fcba45c07cb87db5a0
Daniel Black <daniel.l.black@usace.army.mil>
2023-04-20 08:11:33 -0700

    Add Map to project screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./css/bootstrap/bootstrap.superhero.min.css";
+import "./css/bootstrap/bootstrap.cyborg.min.css";
 import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
