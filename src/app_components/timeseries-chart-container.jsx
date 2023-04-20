472b3ce4e8c8006fde9c6e29f2bad43951b821f2
Daniel Black <daniel.l.black@usace.army.mil>
2023-04-20 12:59:07 -0700

    Refactor TimeseriesChart

---
+++ src/app_components/timeseries-chart-container.jsx
@@ -0,0 +1,59 @@
+import Chart from "./chart";
+import useAPI from "../app_hooks/useAPI";
+import Loader from "./loader";
+import { useState } from "react";
+
+function TimeseriesChartContainer({ location }) {
+  const [tsChecked, setTsChecked] = useState("");
+
+  const timeseries = useAPI("catalog/TIMESERIES", {
+    like: `${location.name}.*`,
+    office: location["office-id"],
+  });
+
+  if (Array.isArray(timeseries)) return <Loader />;
+
+  const data = [];
+  return (
+    <>
+      <div className="col-3">
+        <div className="form-group">
+          {timeseries.entries
+            .filter((ts) => {
+              return !!ts.extents.length;
+            })
+            .sort((a, b) => {
+              if (a.name > b.name) return 1;
+              if (a.name < b.name) return -1;
+              return 0;
+            })
+            .map((ts, i) => {
+              return (
+                <div key={ts.name} className="form-check">
+                  <label className="form-check-label">
+                    <input
+                      type="radio"
+                      className="form-check-input"
+                      name="ts-radio"
+                      id={`ts-radio-${i}`}
+                      value={ts.name}
+                      checked={ts.name === tsChecked}
+                      onChange={(e) => {
+                        setTsChecked(e.currentTarget.value);
+                      }}
+                    />
+                    {ts.name}
+                  </label>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="col-9">
+        <Chart data={data} />;
+      </div>
+    </>
+  );
+}
+
+export default TimeseriesChartContainer;
